f669098b48b2857fa4dfd505c03c4db4c306ff6c
NoahErgo <94392306+NoahErgo@users.noreply.github.com>
2022-02-20 11:05:46 -0800

    Change bot to anetaBTC

--- bot.py
+++ bot.py
@@ -10,36 +10,30 @@ dotenv.load_dotenv()
 
 messages = {
     "hello": "Hey, How's it going?",
-    "faq": """
-You can read the FAQ <a href=\"https://ergopad.io/faq\">here</a> ❓
+    "docs": """
+You can read the projects docs <a href=\"https://docs.anetabtc.io/docs/about-anetabtc/Introduction">here</a> ❓
 """,
     "report": """
 Please be aware that we will never ask for private keys over DM.
 Report any suspicious activity to an admin. If you need assistance, we are able to help by checking your public wallet address.
 Keep your private keys private and never share them with anyone 🚨
 """,
-    "refund": """
-If your transaction stuck and you have to refund your Transaction, make sure to watch our tutorial on Youtube in which Marty explains how to make a Refund.
-
-https://www.youtube.com/watch?v=OTcCNNCnS4Q
-""",
     "socials": """
-<b>Ergopad socials:</b>
-→ <a href=\"https://ergopad.io\">Website</a>
-→ <a href=\"https://github.com/ergo-pad/ergopad/blob/dev/docs/README.md\">Whitepaper</a>
-→ <a href=\"https://twitter.com/ErgoPadOfficial\">Twitter</a>
-→ <a href=\"https://github.com/ergo-pad\">Github</a>
-→ <a href=\"https://discord.com/invite/E8cHp6ThuZ\">Discord</a>
+<b>anetaBTC socials:</b>
+→ <a href=\"http://anetabtc.io/">Website</a>
+→ <a href=\"https://medium.com/@anetaBTC/anetabtc-litepaper-v1-0-171f29b3276a">Litepaper</a>
+→ <a href=\"https://twitter.com/anetaBTC">Twitter</a>
+→ <a href=\"hhttps://github.com/anetabtc">Github</a>
+→ <a href=\"https://discord.com/invite/anetabtc">Discord</a>
 """,
     "welcome": """
-Hello and welcome to the Ergopad community! 🥳  ErgoPad is a project incubator offering token IDOs which provide funding for new projects within the Ergo ecosystem.
-Ergopad will release its own native token through an IDO and users will be able to trade Ergo or SigUSD for these tokens and stake them through smart contracts.
-<b>Ergopad socials:</b>
-→ <a href="https://ergopad.io">Website</a>
-→ <a href="https://github.com/ergo-pad/ergopad/blob/dev/docs/README.md">Whitepaper</a>
-→ <a href="https://twitter.com/ErgoPadOfficial">Twitter</a>
-→ <a href="https://github.com/ergo-pad">Github</a>
-→ <a href="https://discord.com/invite/E8cHp6ThuZ">Discord</a>    
+Hello and welcome to the anetaBTC community! 🥳  anetaBTC is a decentralized, On Chain BTC protocol to unlock the value of Bitcoin on Ergo and Cardano.
+<b>anetaBTC socials:</b>
+→ <a href="http://anetabtc.io/">Website</a>
+→ <a href="https://medium.com/@anetaBTC/anetabtc-litepaper-v1-0-171f29b3276a">Litepaper</a>
+→ <a href="https://twitter.com/anetaBTC">Twitter</a>
+→ <a href="https://github.com/anetabtc">Github</a>
+→ <a href="https://discord.com/invite/anetabtc">Discord</a>    
 """,
     "bot_love": """
 Welcome fellow bot 🥰 😍, I am not sure you belong here though... 👀 👀.
@@ -169,31 +163,26 @@ def price(message):
         # 30 min cooldown
         if message.chat.id in price_last_timestamps and price_last_timestamps[message.chat.id] + 1800 > time.time():
             return
-        res = requests.get(f"{API}/asset/price/ergopad", verify=False)
+        res = requests.get(f"{API}/asset/price/neta", verify=False)
         price = round(res.json()["price"], 4)
         bot.send_message(
-            message.chat.id, f"$ERGOPAD trading at ${price} USD")
+            message.chat.id, f"$NETA trading at ${price} USD")
         price_last_timestamps[message.chat.id] = time.time()
     except Exception as e:
         bot.reply_to(message, "Sorry cannot get price data from ergopad api.")
         print(e)
 
 
-@bot.message_handler(commands=["faq"])
+@bot.message_handler(commands=["docs"])
 def faq(message):
     bot.send_message(
-        message.chat.id, messages["faq"], parse_mode="html")
+        message.chat.id, messages["docs"], parse_mode="html")
 
 
 @bot.message_handler(commands=["report"])
 def report(message):
     bot.send_message(
         message.chat.id, messages["report"], parse_mode="html")
-
-
-@bot.message_handler(commands=["refund"])
-def refund(message):
-    bot.send_message(message.chat.id, messages["refund"])
 
 
 @bot.message_handler(commands=["socials"])
